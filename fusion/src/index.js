317eaa6671317e10eecf14e3a1648e4c62a93d4d
rakanishuman <dannynguyen@hotmail.ca>
2021-01-11 21:31:59 -0700

    removed redundant file

--- fusion/src/index.js
+++ fusion/src/index.js
@@ -2,12 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FuseList from './FuseList'
 
-function App() {
-    return (
-      <div className="App">
-       <FuseList></FuseList>
-      </div>
-    );
-  }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
