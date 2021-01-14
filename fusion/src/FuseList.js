a7c42d3de3e15ecfb16f7111c7d85a8d2e3223dd
rakanishuman <dannynguyen@hotmail.ca>
2021-01-13 22:07:40 -0700

    Created modal; linked to  fuzes

--- fusion/src/FuseList.js
+++ fusion/src/FuseList.js
@@ -12,7 +12,7 @@ let fuseObject = {
         name: 'Big Buff other thing'
     },
     fuse3: {
-        date: 'February 1',
+        date: 'February 01',
         name: 'Country Count thingy thing'
     }
 }
@@ -25,14 +25,12 @@ function getMonth(date, type) {
 const FuseList = (props) => {
     const [modal, setModal] = useState('')
 
-    function openModal(){
+    function openModal() {
         setModal('active')
-        console.log({modal})
     }
 
-    function closeModal(){
+    function closeModal() {
         setModal('')
-        console.log({modal})
     }
 
     return (
@@ -40,69 +38,71 @@ const FuseList = (props) => {
             <div className='fuseWeek'>
                 <h1 className='weekDates'>Jan 10-16</h1>
             </div>
-            <div className="ui link card">
-                <div className="image">
-                    <i className="fuseItemDate huge calendar outline icon"></i>
+            <div className="fuzeItem ui link card" onClick={openModal}>
+                <div className="content">
+                    <h2 className="fuzeTitle header">{fuseObject.fuse1.name}</h2>
+                </div>
+                <div className="fuzeDate image">
+                    <i className=" huge calendar outline icon"></i>
                     <div className="date">
                         <p className="month">{getMonth(fuseObject.fuse1.date, 0)}</p>
                         <span className="day">{getMonth(fuseObject.fuse1.date, 1)}</span>
                     </div>
                 </div>
-                <div className="middle aligned content">
-                    <h2 className="header">{fuseObject.fuse1.name}</h2>
-                </div>
             </div>
-            <div className="ui link card">
-                <div className="image">
-                    <i className="fuseItemDate huge calendar outline icon"></i>
+            <div className="fuzeItem ui link card" onClick={openModal}>
+
+                <div className="middle aligned content">
+                    <h2 className="fuzeTitle header">{fuseObject.fuse2.name}</h2>
+                </div>
+                <div className="fuzeDate image">
+                    <i className=" huge calendar outline icon"></i>
                     <div className="date">
                         <p className="month">{getMonth(fuseObject.fuse2.date, 0)}</p>
                         <span className="day">{getMonth(fuseObject.fuse2.date, 1)}</span>
                     </div>
                 </div>
-                <div className="middle aligned content">
-                    <h2 className="header">{fuseObject.fuse2.name}</h2>
-                </div>
+
             </div>
-            <div className="ui link card">
-                <div className="image">
-                    <i className="fuseItemDate huge calendar outline icon"></i>
+            <div className=" fuzeItem ui link card" onClick={openModal}>
+
+                <div className="middle aligned content">
+                    <h2 className="fuzeTitle header">{fuseObject.fuse3.name}</h2>
+                </div>
+                <div className="fuzeDate image">
+                    <i className=" huge calendar outline icon"></i>
                     <div className="date">
                         <p className="month">{getMonth(fuseObject.fuse3.date, 0)}</p>
                         <span className="day">{getMonth(fuseObject.fuse3.date, 1)}</span>
                     </div>
                 </div>
-                <div className="middle aligned content">
-                    <h2 className="header">{fuseObject.fuse3.name}</h2>
-                </div>
-            </div>
-            <button onClick={openModal}>Open Modal</button>
-            <div className={"ui modal "+ modal} >
-                <i className="close icon" onClick={closeModal}></i>
-                <div className="header">
-                    Profile Picture
-                </div>
-                <div className="image content">
-                    <div className="ui medium image">
-                        <img src="/images/avatar/large/chris.jpg"></img>
-                    </div>
-                    <div className="description">
-                        <div className="ui header">We've auto-chosen a profile image for you.</div>
-                        <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
-                        <p>Is it okay to use this photo?</p>
-                    </div>
-                </div>
-                <div className="actions">
-                    <div className="ui black deny button">
-                        Nope
-                    </div>
-                    <div className="ui positive right labeled icon button">
-                        Yep, that's me
-                        <i className="checkmark icon"></i>
-                    </div>
-                </div>
-            </div>
 
+            </div>
+            <div className={"ui page modals dimmer transition " + modal} >
+                <div className={'ui modal transition ' + modal}>
+                    <i className="close icon" onClick={closeModal}></i>
+                    <div className="ui image content">
+                        <img className='centered ui small image' src="https://s2.qwant.com/thumbr/0x380/f/2/a2d510008beabb9b85c8419f334814b052e00070af6d7cca6b218b9626ca3e/thumbs%20up%20child.jpg?u=https%3A%2F%2Fstatic.ecpbuilder.com%2Fclipart%2Fpeople%2Fthumbs%2520up%2520child.jpg&q=0&b=1&p=0&a=1" alt='random ting'></img>
+                    </div>
+                    <div className="header">
+                        Example Fuze
+                    </div>
+                    <div className="content">
+                        <div className="description">
+                            <div className="ui header">Description</div>
+                            <p>Random poop. All the poop. Never-ending poop. Where did all this poop come from? Someone please help. I am literally drowning in poop. Oh no, it's gotten into my sneakers. I paid $300 for these replica Nike shows from Back to the Future. Dear God, please save me from this poopy suffering.</p>
+                        </div>
+                        <div className='modalContent'>
+                        <div className='linkContent'>
+                        <div className="ui header">Links</div>
+                        <a href='#'>www.silly.com</a><br></br>
+                        <a href='#'>www.goose.com</a>
+                        </div>
+                        <img className='ui small middle aligned image' src='https://s2.qwant.com/thumbr/0x380/d/5/edc623b484dfe71f16269638ec033424a016e6097506cbc5cb7122e15bc6e8/wordpress-google-maps-performance.png?u=https%3A%2F%2Fkinsta.com%2Fwp-content%2Fuploads%2F2019%2F01%2Fwordpress-google-maps-performance.png&q=0&b=1&p=0&a=1'></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div >
     )
 }
--- fusion/src/index.js
+++ fusion/src/index.js
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div className="LandingPage">
       <Text />
+      <FuseList></FuseList>
     </div>
   );
 };
