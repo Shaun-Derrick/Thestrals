1b394bbeed99cc39bc1c68c7a9ee5f8c7f0102cb
Danny Nguyen <dannynguyentechie@gmail.com>
2021-02-17 10:41:32 -0700

    fixed PUT call on Edit

--- fusion/client/src/components/admin/AdminHome.js
+++ fusion/client/src/components/admin/AdminHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Logo from "../Logo"
 import AdminNav from "./AdminNav"
 import AdminText from "./AdminText"
--- fusion/client/src/components/admin/Edit.js
+++ fusion/client/src/components/admin/Edit.js
@@ -26,34 +26,29 @@ const EditFuze = () => {
     //destructuring of fuze
     const { startDate, endDate, startTime, endTime, Title, description, venue, tags, organizer, whereIFoundThisEvent2 } = fuze;
 
-
-
     useEffect(() => {
         //to load a specific fuze for edit
         //so we use get
 
         const loadFuze = async () => {
             const result = await axios.get(`/Fuzes/${_id}`);
-            console.log(`Fuze i want to edit based on id:`, result);
             setFuze(result.data);
         }
         loadFuze()
     }, []);
 
-
     const onInputChange = e => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setFuze({ ...fuze, [e.target.name]: e.target.value });
     };
-
 
     // to show a specfic fuze's details that we want to edit
     const onSubmit = async e => {
         e.preventDefault();
-        console.log(fuze)
         await axios.put(`/Fuzes/${_id}`, fuze);
         history.push("/admin/update");
     };
+
     return (
         <div>
             <Segment.Group>
@@ -61,57 +56,52 @@ const EditFuze = () => {
                     <Header as='h1' content='Edit fuze' color='red' textAlign='center' />
                 </Segment>
                 <Segment inverted>
-                    <Form inverted onSubmit={e => onSubmit(e)}>
+                    <Form inverted>
                         <Form.Field>
                             <Label color='red' horizontal >startDate</Label>
-                            <input type="text" placeholder="startDate" name="startDate" value={startDate||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder="startDate" name="startDate" value={startDate || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>endDate</Label>
-                            <input type="text" placeholder='endDate' name="endDate" value={endDate||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='endDate' name="endDate" value={endDate || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>startTime</Label>
-                            <input type="text" placeholder='startTime' name="startTime" value={startTime||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='startTime' name="startTime" value={startTime || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>endTime</Label>
-                            <input type="text" placeholder='endTime' name="endTime" value={endTime||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='endTime' name="endTime" value={endTime || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>Title</Label>
-                            <input type="text" placeholder='Title' name="Title" value={Title||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='Title' name="Title" value={Title || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>description</Label>
-                            <input type="text" placeholder='description' name="description" value={description||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='description' name="description" value={description || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>venue</Label>
-                            <input type="text" placeholder='venue' name="venue" value={venue||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='venue' name="venue" value={venue || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>tags</Label>
-                            <input type="text" placeholder='tags' name="tags" value={tags||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='tags' name="tags" value={tags || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
 
                         <Form.Field>
 
                             <Label color='red' horizontal>organizer</Label>
-                            <input type="text" placeholder='organizer' name="organizer" value={organizer||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='organizer' name="organizer" value={organizer || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
                         <Form.Field>
                             <Label color='red' horizontal>whereIFoundThisEvent2</Label>
-                            <input type="text" placeholder='whereIFoundThisEvent2' name="whereIFoundThisEvent2" value={whereIFoundThisEvent2||""} onChange={e => onInputChange(e)} />
+                            <input type="text" placeholder='whereIFoundThisEvent2' name="whereIFoundThisEvent2" value={whereIFoundThisEvent2 || ""} onChange={e => onInputChange(e)} />
                         </Form.Field>
-
-                        {/* <Form.Field>
-      <Checkbox Label='Are you sure?' />
-    </Form.Field> */}
-                        <Button animated='fade' color='green' as={Link} to="/admin/update">
+                        <Button animated='fade' type="submit" color='green' onClick={e => { onSubmit(e) }} as={Link} to="/admin/update">
                             <Button.Content visible  >CONFIRM</Button.Content>
                             <Button.Content hidden>Are you sure ? </Button.Content>
-
                         </Button>
                     </Form>
                 </Segment>
