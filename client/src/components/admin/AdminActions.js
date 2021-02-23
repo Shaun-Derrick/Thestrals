1473d804f3d654133f90cc0a012080951819328e
Shaun-Derrick <derrickshaun@gmail.com>
2021-02-22 19:39:46 -0700

    Updated Contact Form

--- client/src/components/admin/AdminActions.js
+++ client/src/components/admin/AdminActions.js
@@ -1,24 +1,17 @@
 import React from "react"
-import { Segment } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 
 const AdminActions = () => {
   return (
     <div id="adminActionsSegment">
-      <Link to="/admin/create">
-        <Segment stacked id="AdminActionOption">
-          Create a Fuze
-        </Segment>
+      <Link to="/admin/create" id="AdminActionOption">
+        Create a Fuze
       </Link>
-      <Link to="/admin/update">
-        <Segment stacked id="AdminActionOption">
-          Update a Fuze
-        </Segment>
+      <Link to="/admin/update" id="AdminActionOption">
+        Update a Fuze
       </Link>
-      <Link to="/admin/delete">
-        <Segment stacked id="AdminActionOption">
-          Delete a Fuze
-        </Segment>
+      <Link to="/admin/delete" id="AdminActionOption">
+        Delete a Fuze
       </Link>
     </div>
   )
